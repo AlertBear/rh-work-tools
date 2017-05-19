d25391e9692b0956c461ee7fa865a4e307ea0b95
dguo <dguo@redhat.com>
2017-05-19 17:53:49 +0800

    error fix

--- vdsm-network-setup/setup.py
+++ vdsm-network-setup/setup.py
@@ -1,9 +1,8 @@
-# !/usr/bin/python2.7
+#!/usr/bin/python2.7
 
 
 import os
 import sys
-import shutil
 import subprocess
 from sysinfo import NETWORK_SYS
 
@@ -27,7 +26,7 @@ def execute(cmd, check=False):
 
 def update_kv(file, kv):
     for k, v in kv.items():
-        cmd = "sed -i 's/{key}=.*/=\"{value}\"/' {file}".format(
+        cmd = "sed -i 's/{key}=.*/{key}=\"{value}\"/' {file}".format(
             key=k, value=v, file=file)
         execute(cmd)
 
@@ -38,40 +37,47 @@ def setup_bond(bond_name, slave1, slave2):
     tpl_path = tpl_dir + "bond"
 
     # Copy the templates to the tmp dir
-    tmp_dir = "/tmp/vdsm-network-setup"
-    if not os.path.exists(tmp_dir):
-        os.makedirs(tmp_dir)
-    shutil.copytree(tpl_path, tmp_dir)
+    tmp_tpl_dir = "/tmp/vdsm-network-setup/bond/"
+    if not os.path.exists(tmp_tpl_dir):
+        os.makedirs(tmp_tpl_dir)
+    else:
+        cmd = "rm -f %sifcfg-*" % tmp_tpl_dir
+        execute(cmd)
 
-    tmp_tpl_dir = tmp_dir + "/bond"
-    bond_cfg_file = tmp_tpl_dir + "ifcfg-bond0"
-    slave1_cfg_file = tmp_tpl_dir + "ifcfg-slave1"
-    slave2_cfg_file = tmp_tpl_dir + "ifcfg-slave2"
+    #shutil.copytree(tpl_path, tmp_dir)
+    cmd = "cp -f %s/ifcfg-* %s" % (tpl_path, tmp_tpl_dir)
+    execute(cmd)
 
     # Get the mac address of slaves
     cmd = "ip link show %s|grep 'link/ether'" % slave1
     output = execute(cmd)
-    slave1_mac = output.split(1)
+    slave1_mac = output.split()[1]
     cmd = "ip link show %s|grep 'link/ether'" % slave2
     output = execute(cmd)
-    slave2_mac = output.split(1)
+    slave2_mac = output.split()[1]
 
     # Rename the template according to the actual variables
-    os.rename(bond_cfg_file, tmp_tpl_dir + "ifcfg-%s" % bond_name)
-    os.rename(slave1_cfg_file, tmp_tpl_dir + "ifcfg-%s" % slave1)
-    os.rename(slave2_cfg_file, tmp_tpl_dir + "ifcfg-%s" % slave2)
+    bond_cfg_file = tmp_tpl_dir + "ifcfg-bond0"
+    slave1_cfg_file = tmp_tpl_dir + "ifcfg-slave1"
+    slave2_cfg_file = tmp_tpl_dir + "ifcfg-slave2"
+    nbond_cfg_file = tmp_tpl_dir + "ifcfg-%s" % bond_name
+    nslave1_cfg_file = tmp_tpl_dir + "ifcfg-%s" % slave1
+    nslave2_cfg_file = tmp_tpl_dir + "ifcfg-%s" % slave2
+    os.rename(bond_cfg_file, nbond_cfg_file)
+    os.rename(slave1_cfg_file, nslave1_cfg_file)
+    os.rename(slave2_cfg_file, nslave2_cfg_file)
 
     # Update the templates
     bond_cfg_kv = {"DEVICE": bond_name}
-    update_kv(bond_cfg_kv, bond_cfg_file)
+    update_kv(nbond_cfg_file, bond_cfg_kv)
 
     slave1_cfg_kv = {
         "DEVICE": slave1, "HWADDR": slave1_mac, "MASTER": bond_name}
-    update_kv(slave1_cfg_kv, slave1_cfg_file)
+    update_kv(nslave1_cfg_file, slave1_cfg_kv)
 
     slave2_cfg_kv = {
         "DEVICE": slave2, "HWADDR": slave2_mac, "MASTER": bond_name}
-    update_kv(slave2_cfg_kv, slave2_cfg_file)
+    update_kv(nslave2_cfg_file, slave2_cfg_kv)
 
     # Move the files to /etc/sysconfig/network-scripts
     network_scripts_dir = "/etc/sysconfig/network-scripts/"
@@ -83,12 +89,8 @@ def setup_bond(bond_name, slave1, slave2):
         os.rename(
             network_scripts_dir + "ifcfg-%s" % slave2,
             network_scripts_dir + "ifcfg-%s.bak" % slave2)
-    cmd = "mv %s/ifcfg-* %s/" % (tmp_tpl_dir, network_scripts_dir)
+    cmd = "mv %sifcfg-* %s" % (tmp_tpl_dir, network_scripts_dir)
     execute(cmd)
-
-    # Restart network service
-    cmd = "ll %s" % network_scripts_dir
-    print execute(cmd)
 
 
 def setup_vlan(vlan_device, vlan_id):
@@ -97,31 +99,36 @@ def setup_vlan(vlan_device, vlan_id):
     tpl_path = tpl_dir + "vlan"
 
     # Copy the templates to the tmp dir
-    tmp_dir = "/tmp/vdsm-network-setup"
-    if not os.path.exists(tmp_dir):
-        os.makedirs(tmp_dir)
-    shutil.copytree(tpl_path, tmp_dir)
+    tmp_tpl_dir = "/tmp/vdsm-network-setup/vlan/"
+    if not os.path.exists(tmp_tpl_dir):
+        os.makedirs(tmp_tpl_dir)
+    else:
+        cmd = "rm -f %sifcfg-*" % tmp_tpl_dir
+        execute(cmd)
 
-    tmp_tpl_dir = tmp_dir + "/vlan"
-    nic_cfg_file = tmp_tpl_dir + "ifcfg-nic1"
-    vlan_cfg_file = tmp_tpl_dir + "ifcfg-nic1.00"
+    # shutil.copytree(tpl_path, tmp_dir)
+    cmd = "cp -f %s/ifcfg-* %s" % (tpl_path, tmp_tpl_dir)
+    execute(cmd)
 
     # Get the mac address of slaves
     cmd = "ip link show %s|grep 'link/ether'" % vlan_device
     output = execute(cmd)
-    nic_mac = output.split(1)
+    nic_mac = output.split()[1]
 
     # Rename the template according to the actual variables
-    os.rename(nic_cfg_file, tmp_tpl_dir + "ifcfg-%s" % vlan_device)
-    os.rename(vlan_cfg_file, tmp_tpl_dir + "ifcfg-%s.%s" % (
-        vlan_device, vlan_id))
+    nic_cfg_file = tmp_tpl_dir + "ifcfg-nic1"
+    vlan_cfg_file = tmp_tpl_dir + "ifcfg-nic1.00"
+    dnic_cfg_file = tmp_tpl_dir + "ifcfg-%s" % vlan_device
+    dvlan_cfg_file = tmp_tpl_dir + "ifcfg-%s.%s" % (vlan_device, vlan_id)
+    os.rename(nic_cfg_file, dnic_cfg_file)
+    os.rename(vlan_cfg_file, dvlan_cfg_file)
 
     # Update the templates
     nic_cfg_kv = {"DEVICE": vlan_device, "HWADDR": nic_mac}
-    update_kv(nic_cfg_kv, nic_cfg_file)
+    update_kv(dnic_cfg_file, nic_cfg_kv)
 
     vlan_cfg_kv = {"DEVICE": vlan_device + ".%s" % vlan_id}
-    update_kv(vlan_cfg_kv, vlan_cfg_file)
+    update_kv(dvlan_cfg_file, vlan_cfg_kv)
 
     # Move the files to /etc/sysconfig/network-scripts
     network_scripts_dir = "/etc/sysconfig/network-scripts/"
@@ -129,12 +136,8 @@ def setup_vlan(vlan_device, vlan_id):
         os.rename(
             network_scripts_dir + "ifcfg-%s" % slave1,
             network_scripts_dir + "ifcfg-%s.bak" % slave1)
-    cmd = "mv %s/ifcfg-* %s/" % (tmp_tpl_dir, network_scripts_dir)
+    cmd = "mv %sifcfg-* %s" % (tmp_tpl_dir, network_scripts_dir)
     execute(cmd)
-
-    # Restart network service
-    cmd = "ll %s" % network_scripts_dir
-    print execute(cmd)
 
 
 def setup_bv(bond_name, slave1, slave2, vlan_id):
@@ -143,45 +146,52 @@ def setup_bv(bond_name, slave1, slave2, vlan_id):
     tpl_path = tpl_dir + "bv"
 
     # Copy the templates to the tmp dir
-    tmp_dir = "/tmp/vdsm-network-setup"
-    if not os.path.exists(tmp_dir):
-        os.makedirs(tmp_dir)
-    shutil.copytree(tpl_path, tmp_dir)
-
-    tmp_tpl_dir = tmp_dir + "/bv"
-    bond_cfg_file = tmp_tpl_dir + "ifcfg-bond1"
-    slave1_cfg_file = tmp_tpl_dir + "ifcfg-slave1"
-    slave2_cfg_file = tmp_tpl_dir + "ifcfg-slave2"
-    vlan_cfg_file = tmp_tpl_dir + "ifcfg-bond1.00"
+    tmp_tpl_dir = "/tmp/vdsm-network-setup/bv/"
+    if not os.path.exists(tmp_tpl_dir):
+        os.makedirs(tmp_tpl_dir)
+    else:
+        cmd = "rm -f %sifcfg-*" % tmp_tpl_dir
+        execute(cmd)
+    # shutil.copytree(tpl_path, tmp_dir)
+    cmd = "cp -f %s/ifcfg-* %s" % (tpl_path, tmp_tpl_dir)
+    execute(cmd)
 
     # Get the mac address of slaves
     cmd = "ip link show %s|grep 'link/ether'" % slave1
     output = execute(cmd)
-    slave1_mac = output.split(1)
+    slave1_mac = output.split()[1]
     cmd = "ip link show %s|grep 'link/ether'" % slave2
     output = execute(cmd)
-    slave2_mac = output.split(1)
+    slave2_mac = output.split()[1]
 
     # Rename the template according to the actual variables
-    os.rename(bond_cfg_file, tmp_tpl_dir + "ifcfg-%s" % bond_name)
-    os.rename(slave1_cfg_file, tmp_tpl_dir + "ifcfg-%s" % slave1)
-    os.rename(slave2_cfg_file, tmp_tpl_dir + "ifcfg-%s" % slave2)
-    os.rename(vlan_cfg_file, tmp_tpl_dir + "ifcfg-%s.%s" % (bond_name, vlan_id))
+    bond_cfg_file = tmp_tpl_dir + "ifcfg-bond1"
+    slave1_cfg_file = tmp_tpl_dir + "ifcfg-slave1"
+    slave2_cfg_file = tmp_tpl_dir + "ifcfg-slave2"
+    vlan_cfg_file = tmp_tpl_dir + "ifcfg-bond1.00"
+    dbond_cfg_file = tmp_tpl_dir + "ifcfg-%s" % bond_name
+    dslave1_cfg_file = tmp_tpl_dir + "ifcfg-%s" % slave1
+    dslave2_cfg_file = tmp_tpl_dir + "ifcfg-%s" % slave2
+    dvlan_cfg_file = tmp_tpl_dir + "ifcfg-%s.%s" % (bond_name, vlan_id)
+    os.rename(bond_cfg_file, dbond_cfg_file)
+    os.rename(slave1_cfg_file, dslave1_cfg_file)
+    os.rename(slave2_cfg_file, dslave2_cfg_file)
+    os.rename(vlan_cfg_file, dvlan_cfg_file)
 
     # Update the templates
     bond_cfg_kv = {"DEVICE": bond_name}
-    update_kv(bond_cfg_kv, bond_cfg_file)
+    update_kv(bond_cfg_file, bond_cfg_kv)
 
     slave1_cfg_kv = {
         "DEVICE": slave1, "HWADDR": slave1_mac, "MASTER": bond_name}
-    update_kv(slave1_cfg_kv, slave1_cfg_file)
+    update_kv(slave1_cfg_file, slave1_cfg_kv)
 
     slave2_cfg_kv = {
         "DEVICE": slave2, "HWADDR": slave2_mac, "MASTER": bond_name}
-    update_kv(slave2_cfg_kv, slave2_cfg_file)
+    update_kv(slave2_cfg_file, slave2_cfg_kv)
 
     vlan_cfg_kv = {"DEVICE": vlan_device + ".%s" % vlan_id}
-    update_kv(vlan_cfg_kv, vlan_cfg_file)
+    update_kv(vlan_cfg_file, vlan_cfg_kv)
 
     # Move the files to /etc/sysconfig/network-scripts
     network_scripts_dir = "/etc/sysconfig/network-scripts/"
@@ -193,12 +203,8 @@ def setup_bv(bond_name, slave1, slave2, vlan_id):
         os.rename(
             network_scripts_dir + "ifcfg-%s" % slave2,
             network_scripts_dir + "ifcfg-%s.bak" % slave2)
-    cmd = "mv %s/ifcfg-* %s/" % (tmp_tpl_dir, network_scripts_dir)
+    cmd = "mv %sifcfg-* %s" % (tmp_tpl_dir, network_scripts_dir)
     execute(cmd)
-
-    # Restart network service
-    cmd = "ll %s" % network_scripts_dir
-    print execute(cmd)
 
 
 if __name__ == "__main__":
@@ -214,15 +220,14 @@ if __name__ == "__main__":
     if len(sys.argv) < 3:
         cmd = "hostname"
         output = execute(cmd)
-        machine_used = output
+        machine_used = output.strip()
     elif len(sys.argv) == 3:
         machine_used = sys.argv[2]
 
     if machine_used not in NETWORK_SYS.keys():
-        print "ERROR: Hostname [%s] not in record, \
-            please see the detail of sysinfo.py \
-            and specify the machine as the second\
-            parameter follow this script" % machine_used
+        print "ERROR: Hostname [%s] not in record, please see the detail\
+        of sysinfo.py and specify the machine as the second parameter follow\
+        this script" % machine_used
         sys.exit(1)
 
     if network_mode == "bond":
